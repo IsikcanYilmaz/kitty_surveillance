501824c183a5189c056ebc5f2fe80119b288abb1
jon <can158@gmail.com>
2019-12-29 21:20:01 -0800

    updating dependencies script

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,16 @@
 
+sudo apt-get update
+
 # PyQt5
+echo "[*] Installing PyQt5"
 # pip3 install PyQt5
 
 # pigpio
-git clone https://github.com/joan2937/pigpio
+# git clone https://github.com/joan2937/pigpio
+echo "[*] Installing pigpio"
+sudo apt-get install pigpio
 pip3 install pigpio
+
+# Set up pigpiod service to be run on startup
+
+
